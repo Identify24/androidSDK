49ea6ff73c5dc228bf591102e982a068e5ad6bad
Evren Ay <evrenay16@gmail.com>
2021-01-18 12:27:39 +0300

    -bugfix

--- app/src/main/java/com/identify/sdk/MainActivity.kt
+++ app/src/main/java/com/identify/sdk/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        IdentifySdk.getInstance().startIdentification(this,"276ddff8-280b-11eb-a693-005056bb3f3f")
+        
     }
 
 
